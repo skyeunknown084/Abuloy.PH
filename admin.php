af77a0b726f221f8dadaa03403af771184a17b8d
Jessele Del Mundo <skyeunknown084@gmail.com>
2022-09-11 20:58:41 +0800

    update admin.php

--- admin.php
+++ admin.php
@@ -3,49 +3,6 @@
 session_start();
 error_reporting(0);
 
-// if(isset($admintype) == 1){
-//     $sql = "SELECT * FROM abuloy_users WHERE id = $adminid";
-
-//     $result = $mysqli->query($sql);
-
-//     $admin = $result->fetch_assoc();
-//         $log_status = $admin['log_status'];
-//     if($log_status == 1){
-//         echo 'success login';
-//         echo $admin['firstname'];
-//         echo $admin['user_type'];
-//     }else{
-//         echo 'failed login';
-//     }
-// }else{
-
-//     $
-
-//     // header('Location: /login');
-//     // session_unset();
-//     // session_destroy();
-// }
-// if(isset($_SESSION['user_id'])) {
-    
-//     $sql = "SELECT * FROM abuloy_users
-//             WHERE id = " . $_SESSION['user_id'] ." AND email_status = 1";
-
-//     $result = $mysqli->query($sql);
-
-//     $admin = $result->fetch_assoc();
-
-    
-
-// }
-// else{
-    
-    // if($admin == ''){
-    //     header("Location: /login");
-    // }else{
-    //     header("Location: /");
-    // }
-    
-// }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -66,18 +23,15 @@ include 'head.php';
         $admintype = 3;
         
         if($admintype == 3){
-            $sql = "SELECT * FROM abuloy_users WHERE user_type = 3 AND log_status = 1 AND id = $admin_id";
+            $sql = "SELECT * FROM abuloy_users WHERE user_type = $admintype AND id = $admin_id";
             $result = $mysqli->query($sql);
             $admin = $result->fetch_assoc();
-            session_regenerate_id();
             include 'header-admin.php';
-            include 'views/dashboard.php';
+            include 'views/dashboard-admin.php';
         }
         else{
             include 'header.php';
             include 'views/dashboard.php';
-            $admintype = 2;
-            $_SESSION['user_type'] = 2;
         }
         
 
